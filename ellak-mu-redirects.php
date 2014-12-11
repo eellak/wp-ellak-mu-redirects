87b66d4ba78151a89033297bfb8c08578f8f1675
Manolis Tzanidakis <mtzanidakis@gmail.com>
2014-12-11 15:06:54 +0200

    make it actually work :)

--- ellak-mu-redirects.php
+++ ellak-mu-redirects.php
@@ -10,9 +10,16 @@ Author URI:  http://mtzanidakis.com/
 License:     ISC
 */
 
-function ellak_login_redirect( $redirect_to, $request_redirect_to, $user ) {
-	// remove wp-admin from the redirect link
-	$redirect_to = preg_replace( '/\/wp-admin\//', '', $redirect_to ); 
+function ellak_login_redirect( $redirect_to, $request, $user ) {
+	global $user;
+
+	// remove wp-admin from the redirect link on subscribers
+	if( isset( $user->roles ) && is_array( $user->roles )
+		&& in_array( 'subscriber', $user->roles ) )
+	{
+		$redirect_to = preg_replace( '/\/wp-admin\//', '',
+			$redirect_to );
+	}
 
 	return $redirect_to;
 } 
